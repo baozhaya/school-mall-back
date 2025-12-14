5f17a9ca0dd0f3f333b1771a1ea0f72b70715153
xiaohong <3095973960@qq.com>
2025-12-14 16:36:46 +0800

    分页查询

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import com.example.common.Result;
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import com.example.service.UserService;
+import com.github.pagehelper.PageInfo;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserMapper userMapper;
+    @Autowired
+    private UserService userService;
+
+    /**
+     *
+     * @param pageNum 当前页码
+     * @param pageSize 每页展示的个数
+     *@return 分页数据
+     * 接口的请求方式：http://localhost:8081/user/selectPage?PageNum=1&Pagesize=10
+     */
+    @GetMapping("/selectPage")
+    public Result selectPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                             @RequestParam(defaultValue = "10") Integer pageSize){
+       PageInfo<User> pageInfo =userService.selectPage(pageNum,pageSize);
+       return Result.success(pageInfo);
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,71 @@
+package com.example.entity;
+
+import java.math.BigDecimal;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+    private String role;
+    private BigDecimal account;
+    private String username;
+    private String avatar;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public BigDecimal getAccount() {
+        return account;
+    }
+
+    public void setAccount(BigDecimal account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
