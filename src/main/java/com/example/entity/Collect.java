c758514fd5f60f5ad6ed0ebb42039c4d7519c339
xiaohong <3095973960@qq.com>
2026-01-14 19:31:39 +0800

    2026.1.14

---
+++ src/main/java/com/example/entity/Collect.java
@@ -0,0 +1,81 @@
+package com.example.entity;
+
+public class Collect {
+
+    /**ID */
+    private Integer id;
+    /**商品ID */
+    private Integer goodsId;
+    private String goodsName;
+    /**用户ID */
+    private Integer userId;
+    private String userName;
+    /**收藏时间 */
+    private String time;
+    private Integer goodsImg;
+    private String goodsPrice;
+
+    public Integer getGoodsImg() {
+        return goodsImg;
+    }
+
+    public void setGoodsImg(Integer goodsImg) {
+        this.goodsImg = goodsImg;
+    }
+
+    public String getGoodsPrice() {
+        return goodsPrice;
+    }
+
+    public void setGoodsPrice(String goodsPrice) {
+        this.goodsPrice = goodsPrice;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ src/main/java/com/example/service/GoodsService.java
@@ -0,0 +1,73 @@
+package com.example.service;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.example.entity.Account;
+import com.example.entity.Goods;
+import com.example.exception.CustomException;
+import com.example.mapper.GoodsMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 业务处理
+ **/
+@Service
+public class GoodsService {
+
+    @Resource
+    private GoodsMapper goodsMapper;
+
+    /**
+     * 新增
+     */
+    public void add(Goods goods) {
+        goods.setViews(0);
+        goods.setSaleCount(0);
+        String now = DateUtil.now();//当前最新时间
+        goods.setTime(now);
+        goodsMapper.insert(goods);
+    }
+
+    /**
+     * 删除
+     */
+    public void deleteById(Integer id) {
+        goodsMapper.deleteById(id);
+    }
+
+    /**
+     * 修改
+     */
+    public void updateById(Goods goods) {
+        goodsMapper.updateById(goods);
+    }
+
+    /**
+     * 根据ID查询
+     */
+    public Goods selectById(Integer id) {
+        return goodsMapper.selectById(id);
+    }
+
+    /**
+     * 查询所有
+     */
+    public List<Goods> selectAll(Goods goods) {
+        return goodsMapper.selectAll(goods);
+    }
+
+    /**
+     * 分页查询
+     */
+    public PageInfo<Goods> selectPage(Goods goods, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Goods> list = goodsMapper.selectAll(goods);
+        return PageInfo.of(list);
+    }
+
+}
